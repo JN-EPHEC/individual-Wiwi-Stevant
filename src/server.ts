b80db992c774c935e27904240d24f1f095def8c2
Wiwi-Stevant <he202476@students.ephec.be>
2026-02-12 18:58:01 +0200

    route avec param (ex 6.2)

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import console = require('node:console');
 
 const app = express();
 const port = 3000; 
 let print = console.log
+const etudiants = [
+    { id: 1, nom: "Dupont", prenom: "Jean"},
+    { id: 2, nom: "Martin", prenom: "Sophie"},
+    { id: 3, nom: "Doe", prenom: "Jhon"}
+]
 
-app.get('/', (req : object, res : object) => {
+app.get('/', (req : Request, res : Response ) => {
     res.send("Bienvenue sur mon serveur API");
+});
+
+app.get('/api/data', (req : Request, res : Response) => {
+    res.json(etudiants);
+});
+
+app.get('/api/hello/:name', (req : Request, res : Response) => {
+    let retours = {message : `bonjours ${req.params.name}`, timestamp : new Date}
+    res.json(retours);
 });
 
 app.listen(port,() => {
