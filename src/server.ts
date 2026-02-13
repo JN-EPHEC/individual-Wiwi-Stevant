d430b40c85ad7881edb0ac74e5919eb9d68c5fd3
Wiwi-Stevant <he202476@students.ephec.be>
2026-02-13 19:35:51 +0200

    models user

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import console = require('node:console');
 import userRoutes from "./routes/userRoutes";
+import sequelize from "./config/database";
 
 const app = express();
 const port = 3000; 
@@ -29,3 +29,15 @@ app.use("/api", userRoutes);
 app.listen(port,() => {
     console.log(`serveur lancé sur http://localhost:${port}`);
 });
+
+async function testConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log("tt fonctionne")
+    }
+    catch (error) {
+        console.error("il y a un probleme avec la connection db" + error);
+    }
+}
+
+testConnection()
