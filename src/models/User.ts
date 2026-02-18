d4b16dbdf8569e3d89445aad628ebc4a9a5d28f6
Wiwi-Stevant <he202476@students.ephec.be>
2026-02-18 18:06:36 +0100

    tp2.3

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,25 @@
 import { DataTypes, Model } from 'sequelize';
 import sequelize from '../config/database';
 
-class User extends Model {
-  public id!: number;
-  public firstName!: string;
-  public lastName?: string;
-}
+class User extends Model {}
 
 User.init(
   {
+    // Model attributes are defined here
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     lastName: {
       type: DataTypes.STRING,
+      // allowNull defaults to true
     },
   },
   {
-    sequelize,
-    modelName: 'User',
-    tableName: 'Users',
-    timestamps: false,
-  }
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+    modelName: 'User', // We need to choose the model name
+  },
 );
 
 export default User;
